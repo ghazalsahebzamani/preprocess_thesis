a00d689529c291d9392ac009830850b1aa2b11fc
Ghazal Sahebzamani <ghazal.sahebzamani@gmail.com>
2020-01-30 21:55:17 -0800

    Update analyze_lv.py

--- analyze_lv.py
+++ analyze_lv.py
@@ -1,10 +1,7 @@
 
 import os
 import numpy as np
-# import mysql.connector
-# import pydicom
 import scipy.io as sio
-# import datetime
 LINE_NUM=44
 f2=open('/media/ghazal/New Volume/dist_to_measures6_10.txt',"r")
 f3=open('/media/ghazal/New Volume/col_indcs6_10.txt',"r")
@@ -16,8 +13,6 @@ for i in range(LINE_NUM):
     dists = np.zeros([len(arr)-2])
     for j in range(len(arr)-2): #to ignore the last '\n'
         dists[j]=arr[j+1]
-    # print(arr2[np.argmin(dists)+1])
-    # print(arr[np.argmin(dists)+1])
     lv_ind_list.append(arr2[np.argmin(dists)+1])
 lv2=list(dict.fromkeys(lv_ind_list))
 for i in range(len(lv2)):
